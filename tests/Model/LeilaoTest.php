e2939d27771b0ed4e2bf229754b9f4f76738c75a
Pedro Gonçalves <pedro@unimestre.com>
2021-05-03 12:20:44 -0300

    AULA 5 ATIVIDADE 2 - Testando o leilão

---
+++ tests/Model/LeilaoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Alura\Leilao\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+
+class LeilaoTest extends TestCase
+{
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(
+        int $qtdLances, 
+        Leilao $leilao, 
+        array $valores
+    ){
+        static::assertCount($qtdLances, $leilao->getLances());
+
+        foreach ($valores as $indice => $valorEsperado) {
+            static::assertEquals($valorEsperado, $leilao->getLances()[$indice]->getValor());
+        }
+    }
+
+    public function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Fiat 147 0KM');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Fusca 1972 0KM');
+        $leilaoCom1Lance->recebeLance(new Lance($maria, 5000));
+
+        return [
+            'leilao-2-lances' => [2, $leilaoCom2Lances, [1000, 2000]],
+            'leilao-1-lance' => [1, $leilaoCom1Lance, [5000]]
+        ];
+    }
+}
